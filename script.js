550d7068581d03e90b9054ed1e4444d7d65626bc
Alexandre <159025583+ProfXaxa@users.noreply.github.com>
2025-03-28 19:34:29 +0000

    contraste p3

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function(){
    
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
-    
+
+    const alternaContraste = this.getElementById('alterna-contraste')
 
     botaoDeAcessibilidade.addEventListener('click', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
@@ -20,4 +21,7 @@ document.addEventListener('DOMContentLoaded', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
 });
